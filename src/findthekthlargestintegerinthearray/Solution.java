be0c20b06672b9be5314e5bf4866398b04082f43
Liu Xuxin <liuxuxin@outlook.com>
2022-03-22 17:19:47 +0800

    pass 1985. Find the Kth Largest Integer in the Array

---
+++ src/findthekthlargestintegerinthearray/Solution.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package findthekthlargestintegerinthearray;
+
+import java.util.Arrays;
+
+public class Solution {
+  public String kthLargestNumber(String[] nums, int k) {
+    Arrays.sort(
+        nums,
+        (o1, o2) -> {
+          if (o1.length() == o2.length()) {
+            return o1.compareTo(o2);
+          } else if (o1.length() < o2.length()) {
+            return -1;
+          } else {
+            return 1;
+          }
+        });
+
+    return nums[nums.length - k];
+  }
+
+  public static void main(String[] args) {
+    System.out.println(new Solution().kthLargestNumber(new String[] {"3", "6", "7", "10"}, 1));
+  }
+}
